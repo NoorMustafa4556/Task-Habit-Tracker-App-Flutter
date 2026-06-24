8527819c29c10bac48aaba01ce51d8886530cd03
NoorMustafa4556 <noormustafa4556@gmail.com>
2026-06-24 21:26:34 +0500

    fix(android): comment out ndkVersion to fix build errors due to corrupted NDK installation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.task_and_habit_tracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
